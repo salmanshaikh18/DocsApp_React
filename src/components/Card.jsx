ac3365f225207af61e166209f014911789f0c745
Salman <salmanshaikh18786@gmail.com>
2023-12-07 13:31:35 +0530

    farmer motion

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { FaRegFileAlt } from "react-icons/fa";
 import { MdOutlineFileDownload } from "react-icons/md";
 import { IoIosClose } from "react-icons/io";
+import { motion } from "framer-motion"
 
-const Card = ({data}) => {
+const Card = ({data, reference}) => {
     return (
-        <div className='relative w-60 h-72 bg-zinc-900/90 rounded-[45px] text-white px-5 py-10 overflow-hidden'>
+        <motion.div drag dragConstraints={reference} className='relative flex-shrink-0 w-60 h-72 bg-zinc-900/90 rounded-[45px] text-white px-5 py-10 overflow-hidden'>
             <FaRegFileAlt />
             <p className='text-sm leading-tight mt-5 font-semibold'>{data.description}</p>
             <div className='footer absolute bottom-0 w-full left-0'>
@@ -15,12 +16,11 @@ const Card = ({data}) => {
                         {data.close ? <IoIosClose /> : <MdOutlineFileDownload size=".9em" color="#fff" />}                       
                     </span>
                 </div>
-                {data.tag.isOpen ? <div className='w-full py-4 bg-green-600'>
-                    <h3 className='text-sm font-semibold flex items-center justify-center'>{data.tag.tagTitle}</h3>
-                </div> : ''}
-                
+                {data.tag.isOpen && <div className={`w-full py-4 ${data.tag.tagColor === "blue" ? "bg-blue-600" : "bg-green-600"} flex items-center justify-center`}>
+                    <h3 className='text-sm font-semibold'>{data.tag.tagTitle}</h3>
+                </div>}             
             </div>
-        </div>
+        </motion.div>
     )
 }
 
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react'
+import { useRef } from 'react'
 import Card from './Card'
+import { motion } from "framer-motion"
 
 const Foreground = () => {
 
-    // const data = [
-    //     description, filesize, closeOrDownload, tagdetails
-    // ]
+    const ref = useRef(null)
 
     const data = [
         {
@@ -13,14 +12,25 @@ const Foreground = () => {
             filesize: "0.9mb",
             close: false,
             tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" }
+        },
+        {
+            description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet, at.",
+            filesize: "0.9mb",
+            close: false,
+            tag: { isOpen: true, tagTitle: "Upload", tagColor: "blue" }
+        },
+        {
+            description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet, at.",
+            filesize: "0.9mb",
+            close: true,
+            tag: { isOpen: false, tagTitle: "Download Now", tagColor: "green" }
         }
     ]
 
     return (
-        <div className='fixed top-0 left-0 z-[3] w-full h-full'>
-            {/* <Card /> */}
+        <div ref={ref} className='fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5'>
             {data.map((item, index) => (
-                <Card key={Date.now()} data={item} />
+                <Card key={Date.now()} data={item} reference={ref} />
             ))}
         </div>
     )
