756b68e90ffae887571852f7343f9adf4cb29c3b
Salman <salmanshaikh18786@gmail.com>
2023-12-07 12:44:13 +0530

    update components

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from './Card'
 
 const Foreground = () => {
+
+    // const data = [
+    //     description, filesize, closeOrDownload, tagdetails
+    // ]
+
+    const data = [
+        {
+            description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet, at.",
+            filesize: "0.9mb",
+            close: false,
+            tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" }
+        }
+    ]
+
     return (
         <div className='fixed top-0 left-0 z-[3] w-full h-full'>
-           <Card />
+            {/* <Card /> */}
+            {data.map((item, index) => (
+                <Card key={Date.now()} data={item} />
+            ))}
         </div>
     )
 }
