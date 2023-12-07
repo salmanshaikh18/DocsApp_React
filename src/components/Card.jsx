a0cc3c67281462ebf86612b68006871308227def
Salman <salmanshaikh18786@gmail.com>
2023-12-07 13:39:54 +0530

    farmer motion

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion"
 
 const Card = ({data, reference}) => {
     return (
-        <motion.div drag dragConstraints={reference} className='relative flex-shrink-0 w-60 h-72 bg-zinc-900/90 rounded-[45px] text-white px-5 py-10 overflow-hidden'>
+        <motion.div drag dragConstraints={reference} whileDrag={{scale: 1.1}} dragElastic={1} dragTransition={{ bounceStiffness: 100, bounceDamping: 30 }} className='relative flex-shrink-0 w-60 h-72 bg-zinc-900/90 rounded-[45px] text-white px-5 py-10 overflow-hidden'>
             <FaRegFileAlt />
             <p className='text-sm leading-tight mt-5 font-semibold'>{data.description}</p>
             <div className='footer absolute bottom-0 w-full left-0'>
